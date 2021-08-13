962b350e908416804b986ec0e51564b442388b78
Conrad Ludgate <conradludgate@gmail.com>
2021-08-13 18:42:23 +0100

    init

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    spotify = {
+      version = "~> 0.2.3"
+      source  = "conradludgate/spotify"
+    }
+  }
+}
+
+provider "spotify" {
+  auth_server = "https://oauth2.conrad.cafe"
+  username    = "conradludgate"
+
+  api_key  = var.api_key
+  token_id = var.token_id
+}
+
+variable "api_key" {
+  type = string
+}
+
+variable "token_id" {
+  type = string
+}
